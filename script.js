f1d3aa85f0f6e7aa95012fcca835d0f10e7c5a9b
Mahmud Jamal <mahmudjamal014@gmail.com>
2022-07-28 16:24:24 -0600

    Pressing operators/numbers writes them on the screen

--- script.js
+++ script.js
@@ -25,3 +25,32 @@ function operator(op, x, y) {
         return divide(x, y);
     }
 }
+
+const currentOp = document.querySelector('.current-op');
+const previousOp = document.querySelector('.previous-op');
+
+document.querySelector('.all-clear').onclick = () => {
+    document.querySelector('.previous-op').innerHTML = '';
+    currentOp.innerHTML = '0';
+};
+
+
+document.querySelectorAll('.number').forEach(button => {
+    button.onclick = () => {
+        if (currentOp.innerHTML == '0') {
+            currentOp.innerHTML = button.innerHTML;
+        } else {
+            currentOp.innerHTML += button.innerHTML;
+        }
+    };
+});
+
+document.querySelectorAll('.operator').forEach(button => {
+    button.onclick = () => {
+        if (currentOp.innerHTML != '') {
+            previousOp.innerHTML = currentOp.innerHTML;
+            previousOp.innerHTML += button.innerHTML;
+            currentOp.innerHTML = '';
+        } 
+    };
+});
