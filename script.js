ebb60d4ea3748266205634bf2b8019e1d66ae101
Mahmud Jamal <mahmudjamal014@gmail.com>
2022-07-28 01:13:49 -0600

    Created html page and linked css

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ function multiply(x, y) {
 function divide(x, y) {
     return x / y;
 }
+
+function operator(op, x, y) {
+    if (op === '+') {
+        return add(x, y);
+    } else if (op === '-') {
+        return subtract(x, y);
+    } else if (op === '*') {
+        return multiply(x, y);
+    } else if (op === '/') {
+        return divide(x, y);
+    }
+}
