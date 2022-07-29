9e4a465bea6fa88fc631db641ce9144d23bf30d7
Mahmud Jamal <mahmudjamal014@gmail.com>
2022-07-28 19:37:04 -0600

    Number and operation buttons work; screen outputs as expected

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function operator(op, x, y) {
         return add(x, y);
     } else if (op === '-') {
         return subtract(x, y);
-    } else if (op === '*') {
+    } else if (op === 'X') {
         return multiply(x, y);
     } else if (op === '/') {
         return divide(x, y);
@@ -31,24 +31,29 @@ const previousOp = document.querySelector('.previous-op');
 
 // Initialize var with a number button
 let lastPressed = document.querySelector('.number');
+let lastOperation = undefined;
 let previousNum = 0;
 let currentNum = 0;
 
+
 document.querySelector('.all-clear').onclick = () => {
     document.querySelector('.previous-op').innerHTML = '';
-    currentOp.innerHTML = '0';
+    currentOp.innerHTML = '';
+    previousNum = 0;
+    currentNum = 0;
 };
 
 
 document.querySelectorAll('.number').forEach(button => {
     button.onclick = () => {
-        if (currentOp.innerHTML == '0' || lastPressed.classList.contains('operator')) {
+        if (currentOp.innerHTML == '' || lastPressed.classList.contains('operator')) {
             currentOp.innerHTML = button.innerHTML;
         } else {
             currentOp.innerHTML += button.innerHTML;
         }
         // Change to parseFloat
         currentNum = parseInt(currentOp.innerHTML);
+        //console.log(`currentNum: ${currentNum}`);
         lastPressed = button;
     };
 });
@@ -56,18 +61,22 @@ document.querySelectorAll('.number').forEach(button => {
 document.querySelectorAll('.operator').forEach(button => {
     button.onclick = () => {
         if (!lastPressed.classList.contains('operator')) {
-
-            // this causes problems displaying nums on screen
-            //if (previousOp.innerHTML != '') {
-              //  currentOp.innerHTML = operator(button.innerHTML, previousNum, currentNum);
-            //} 
             
-            console.log(`${currentOp.innerHTML}, ${button.innerHTML}`);
-            previousOp.innerHTML += currentOp.innerHTML + button.innerHTML;
-            //previousOp.innerHTML += button.innerHTML;
+            if (previousOp.innerHTML != '') {
+                console.log(`${previousNum}, ${currentNum}`);
+                previousNum = operator(lastOperation, previousNum, currentNum);
 
-            //console.log(`${previousNum}, ${currentNum}`);
+                console.log(previousNum);
+                currentOp.innerHTML = previousNum;
+            } else {
+                previousNum = currentNum;
+            }
             
+            //console.log(`${currentOp.innerHTML}, ${button.innerHTML}`);
+            previousOp.innerHTML += currentNum + button.innerHTML;
+
+            
+            lastOperation = button.innerHTML;
             lastPressed = button;
         }
     };
